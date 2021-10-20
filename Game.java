5303c02463a4661a421203fe150b3d26e265e117
ckennedy46 <41365122+ckennedy46@users.noreply.github.com>
2021-10-20 15:26:21 -0400

    Create two modes
    Toggle the mode from play to author from the menu bar. Initializes in the author mode, but that can switch if we want to

--- Game.java
+++ Game.java
@@ -1,18 +1,48 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
 public class Game {
 	private JFrame frame;
 	private boolean mode; //true == play mode, false == author mode
+	private JMenuBar menuBar;
+	private JMenu modeMenu;
+	private JRadioButtonMenuItem authorModeMenuItem;
+	private JRadioButtonMenuItem playModeMenuItem;
+	private ButtonGroup modeButtonGroup;
 	private JLabel tempLabel; //label to show play/author mode
 
 	public Game() {
 		frame = new JFrame();
 		mode = false;
+		menuBar = new JMenuBar();
+		modeMenu = new JMenu("Mode");
+		authorModeMenuItem = new JRadioButtonMenuItem("Author", true);
+		playModeMenuItem = new JRadioButtonMenuItem("Play", false);
+		modeButtonGroup = new ButtonGroup();
+
+		authorModeMenuItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent c) {
+				toggleMode();
+			}
+		});
+		playModeMenuItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent c) {
+				toggleMode();
+			}
+		});
+
+		modeButtonGroup.add(authorModeMenuItem);
+		modeButtonGroup.add(playModeMenuItem);
+		modeMenu.add(authorModeMenuItem);
+		modeMenu.add(playModeMenuItem);
+		menuBar.add(modeMenu);
+
 		tempLabel = new JLabel("Author Mode");
 
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setMinimumSize(new Dimension(700, 250)); 
+		frame.setJMenuBar(menuBar);
 		frame.add(tempLabel);
 		frame.pack();
 		frame.setVisible(true);
@@ -24,6 +54,18 @@ public class Game {
 				Game game = new Game();
 			}
 		});
+	}
+
+	private void toggleMode() {
+		if (mode) {
+			mode = false;
+			tempLabel.setText("Author Mode");
+		}
+		else {
+			mode = true;
+			tempLabel.setText("Play Mode");
+		}
+		frame.repaint();
 	}
 }
 
